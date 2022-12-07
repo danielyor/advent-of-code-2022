4afbda59a7d4cd4d93feccd1e99f578dfa7cf207
Daniel Yordanov <22645152+danielyor@users.noreply.github.com>
2022-12-07 15:19:29 +0200

    fix and cleanup day 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Advent of Code 2022 with JavaScript
+
+Open your puzzle input and run the corresponding solution in the browser console.
--- day01/day1.js
+++ day01/day1.js
@@ -1,10 +1,13 @@
+// Advent of Code 2022 - Day 01
+// Open your input and run in the browser console
+
 let input = window.document.getElementsByTagName("pre")[0];
 input = input.innerText.split('\n\n');
 
 for (let i=0; i<input.length; i++) {
 	input[i] = input[i].split('\n').map(Number).reduce( (x,y) => x+y, 0);
 }
-console.log(Math.max(...input));
+console.log("Part One Answer: "+Math.max(...input));
 
-input.reverse().sort();
-console.log(input[0]+input[1]+input[2]);
+input.sort().reverse();
+console.log("Part Two Answer: "+(input[0]+input[1]+input[2]));
